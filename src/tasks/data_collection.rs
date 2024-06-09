fb24cac50a2f53bf5a7a19c31664e64b6dab8f6f
vegewadev <hegewaldvin@gmail.com>
2024-06-09 21:19:52 +0200

    feat: check for doc inserted last 120 mins

--- src/tasks/data_collection.rs
+++ src/tasks/data_collection.rs
@@ -1,13 +1,17 @@
 use chrono::prelude::*;
-use mongodb::{bson::DateTime as BsonDateTime, Client, Collection};
-use serde::Serialize;
-use std::time::Duration;
+use mongodb::{
+    bson::{doc, DateTime as BsonDateTime},
+    options::{FindOneOptions, FindOptions},
+    Client, Collection,
+};
+use serde::{Deserialize, Serialize};
+use std::{panic::UnwindSafe, time::Duration};
 use tokio::time;
 use tracing::{error, info};
 
 use dht11_gpio::{DHT11Controller, Sensor};
 
-#[derive(Serialize)]
+#[derive(Serialize, Deserialize, Debug)]
 struct ClimateMonitoring {
     device_id: String,
     interval_start: BsonDateTime,
@@ -16,23 +20,55 @@ struct ClimateMonitoring {
     metadata: Metadata,
 }
 
-#[derive(Serialize)]
+#[derive(Serialize, Deserialize, Debug)]
 struct ClimateData {
     timestamp: BsonDateTime,
     temperature: f64,
     humidity: f64,
 }
 
-#[derive(Serialize)]
+#[derive(Serialize, Deserialize, Debug)]
 struct Units {
     temperature: String,
     humidity: String,
 }
 
-#[derive(Serialize)]
+#[derive(Serialize, Deserialize, Debug)]
 struct Metadata {
     sensor_type: String,
     installation_date: BsonDateTime,
+}
+
+struct IntervalStartResult {
+    within_last_120_minutes: bool,
+    last_interval_start: Option<DateTime<Utc>>,
+}
+
+async fn check_interval_start_in_last_120_minutes(
+    collection: Collection<ClimateMonitoring>,
+) -> mongodb::error::Result<(bool, Option<ClimateMonitoring>)> {
+    let current_time: DateTime<Utc> = Utc::now();
+    let time_120_minutes_ago: DateTime<Utc> = current_time - chrono::Duration::minutes(120);
+
+    let current_time_millis: i64 = current_time.timestamp_millis();
+    let time_120_minutes_ago_millis: i64 = time_120_minutes_ago.timestamp_millis();
+
+    let filter: mongodb::bson::Document = doc! {
+        "interval_start": {
+            "$gte": BsonDateTime::from_millis(time_120_minutes_ago_millis),
+            "$lte": BsonDateTime::from_millis(current_time_millis),
+        }
+    };
+
+    let find_options = FindOneOptions::builder()
+        .sort(doc! { "interval_start": -1 })
+        .build();
+
+    let result: Option<ClimateMonitoring> = collection.find_one(filter, find_options).await?;
+
+    let is_within_last_120_minutes = result.is_some();
+
+    Ok((is_within_last_120_minutes, result))
 }
 
 pub async fn run(client: Client) {
@@ -53,35 +89,51 @@ pub async fn run(client: Client) {
                 info!("captured temperature: {} °C", data.temperature);
                 info!("captured humidity: {} %", data.humidity);
 
-                let now: DateTime<Utc> = Utc::now();
-                let now_with_offset: DateTime<Utc> = now.with_timezone(&Utc);
-                let millis_since_epoch: i64 = now_with_offset.timestamp_millis();
+                match check_interval_start_in_last_120_minutes(collection.clone()).await {
+                    Ok((is_within_last_120_minutes, result)) => {
+                        info!("Is within last 120 minutes: {}", is_within_last_120_minutes);
+                        if let Some(doc) = result {
+                            info!("Last document: {:?}", doc);
+                        } else {
+                            info!("No document found within last 120 minutes. Creating a new one");
 
-                let monitoring_data: ClimateMonitoring = ClimateMonitoring {
-                    device_id: String::from("sensor-001"),
-                    interval_start: BsonDateTime::from_millis(millis_since_epoch),
-                    data: vec![ClimateData {
-                        timestamp: BsonDateTime::from_millis(millis_since_epoch),
-                        temperature: data.temperature,
-                        humidity: data.humidity,
-                    }],
-                    units: Units {
-                        temperature: String::from("Celsius"),
-                        humidity: String::from("Percentage"),
-                    },
-                    metadata: Metadata {
-                        sensor_type: String::from("DHT11"),
-                        installation_date: BsonDateTime::from_millis(
-                            Utc.ymd(2023, 1, 1).and_hms(0, 0, 0).timestamp_millis(),
-                        ),
-                    },
-                };
+                            let now: DateTime<Utc> = Utc::now();
+                            let now_with_offset: DateTime<Utc> = now.with_timezone(&Utc);
+                            let millis_since_epoch: i64 = now_with_offset.timestamp_millis();
 
-                let result: Result<mongodb::results::InsertOneResult, mongodb::error::Error> =
-                    collection.insert_one(monitoring_data, None).await;
-                match result {
-                    Ok(_) => info!("Data inserted successfully."),
-                    Err(e) => error!("Error inserting data: {}", e),
+                            let monitoring_data: ClimateMonitoring = ClimateMonitoring {
+                                device_id: String::from("sensor-001"),
+                                interval_start: BsonDateTime::from_millis(millis_since_epoch),
+                                data: vec![ClimateData {
+                                    timestamp: BsonDateTime::from_millis(millis_since_epoch),
+                                    temperature: data.temperature,
+                                    humidity: data.humidity,
+                                }],
+                                units: Units {
+                                    temperature: String::from("Celsius"),
+                                    humidity: String::from("Percentage"),
+                                },
+                                metadata: Metadata {
+                                    sensor_type: String::from("DHT11"),
+                                    installation_date: BsonDateTime::from_millis(
+                                        Utc.ymd(2024, 6, 9).and_hms(0, 0, 0).timestamp_millis(),
+                                    ),
+                                },
+                            };
+
+                            let result: Result<
+                                mongodb::results::InsertOneResult,
+                                mongodb::error::Error,
+                            > = collection.insert_one(monitoring_data, None).await;
+                            match result {
+                                Ok(_) => info!("Data inserted successfully."),
+                                Err(e) => error!("Error inserting data: {}", e),
+                            }
+                        }
+                    }
+                    Err(err) => {
+                        eprintln!("Error: {}", err);
+                    }
                 }
             }
             Err(err) => {
