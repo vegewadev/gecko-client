644df74e1081c30892b9769301f29e8592dc04f9
vegewadev <hegewaldvin@gmail.com>
2024-06-09 15:07:22 +0200

    feat: use another crate for dht11 sensor

--- src/tasks/data_collection.rs
+++ src/tasks/data_collection.rs
@@ -1,35 +1,38 @@
-use dht_sensor::Sensor;
-use rppal::gpio::{Gpio, OutputPin};
 use std::error::Error;
 use std::time::Duration;
 use tokio::time;
 use tracing::info;
+
+use rppal::{
+    gpio::{Gpio, Mode},
+    hal::Delay,
+};
+use rppal_dht11::{Dht11, Measurement};
 
 const GPIO_PIN: u8 = 4;
 
 pub async fn run() -> Result<(), Box<dyn Error>> {
     info!("Starting task: data_collections");
 
-    let gpio = Gpio::new()?;
-    let mut information = gpio.get(GPIO_PIN)?.into_output();
-
-    let sensor = Sensor::new(GPIO_PIN)?;
+    let pin = Gpio::new()
+        .unwrap()
+        .get(GPIO_PIN)
+        .unwrap()
+        .into_io(Mode::Output);
+    let mut dht11 = Dht11::new(pin);
+    let mut delay = Delay::new();
 
     loop {
-        match sensor.read() {
-            Ok(data) => {
-                info!(
-                    "Temperature: {:.1}°C, Humidity: {:.1}%",
-                    data.temperature, data.humidity
-                );
+        match dht11.perform_measurement_with_retries(&mut delay, 20) {
+            Ok(Measurement {
+                temperature,
+                humidity,
+            }) => {
+                let (temperature, humidity) = (temperature as f64 / 10.0, humidity as f64 / 10.0);
+                println!("Temp: {temperature:.1}C Hum: {humidity:.1}");
             }
-            Err(e) => {
-                info!("Error reading from sensor: {}", e);
-            }
+            Err(e) => eprintln!("Failed to perform measurement: {e:?}"),
         }
-
-        information.toggle();
-
         time::sleep(Duration::from_secs(10)).await;
     }
 }
