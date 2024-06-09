c57550bea2d1e605d88280fa53f13871b87e9e06
vegewadev <hegewaldvin@gmail.com>
2024-06-09 13:38:11 +0200

    feat: add mongodb Database connection

---
+++ src/database/database.rs
@@ -0,0 +1,48 @@
+use mongodb::{error::Result, options::ClientOptions, Client};
+use tracing::{error, info};
+
+pub struct Database {
+    pub connection_string: String,
+    pub client: Option<Client>,
+}
+
+impl Database {
+    pub async fn connect(connection_string: String) -> Result<Self> {
+        let client_options = match ClientOptions::parse(&connection_string).await {
+            Ok(options) => options,
+            Err(e) => {
+                error!("[ERROR] Failed to parse client options: {:?}", e);
+                return Err(e.into());
+            }
+        };
+
+        let client = match Client::with_options(client_options) {
+            Ok(client) => {
+                info!("[INFO] Successfully created client with options");
+                client
+            }
+            Err(e) => {
+                error!("[ERROR] Failed to create client with options: {:?}", e);
+                return Err(e.into());
+            }
+        };
+
+        // Test database connection
+        match client.list_database_names(None, None).await {
+            Ok(_) => {
+                info!("[INFO] Database connection test succeeded");
+                Ok(Database {
+                    connection_string,
+                    client: Some(client),
+                })
+            }
+            Err(e) => {
+                error!(
+                    "[ERROR] Database connection test failed. Failed to list database names: {:?}",
+                    e
+                );
+                Err(e.into())
+            }
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,36 @@
-fn main() {
-    println!("Hello, world!");
+mod database;
+
+use database::database::Database;
+use dotenv::dotenv;
+use mongodb::error::Result;
+use tokio;
+use tracing::{error, info};
+use tracing_subscriber::FmtSubscriber;
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let subscriber: FmtSubscriber = FmtSubscriber::builder().finish();
+
+    tracing::subscriber::set_global_default(subscriber).expect("setting default subscriber failed");
+
+    dotenv().ok();
+
+    let connection_string = std::env::var("CONNECTION_STRING")
+        .expect("CONNECTION_STRING must be set as an environment variable.");
+
+    match Database::connect(connection_string).await {
+        Ok(db) => {
+            if let Some(_client) = db.client {
+                info!("[INFO] Database connected successfully");
+            } else {
+                error!("[ERROR] Client is None after successful connection");
+            }
+        }
+        Err(e) => {
+            error!("[ERROR] Failed to connect to the database: {:?}", e);
+            return Err(e);
+        }
+    }
+
+    Ok(())
 }
